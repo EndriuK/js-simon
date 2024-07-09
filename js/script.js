14feaecc8ab062b5259a2db2e7e65d9a66c4dafe
EndriuK <endrit.kaskija@gmail.com>
2024-07-09 15:41:01 +0200

    [UPD] ciclo while

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 // ARRAY NUMERI RANDOM
 let randomNumbers = [];
+
+// CICLO WHILE
+while (randomNumbers.length < 5) {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  if (!randomNumbers.includes(randomNumber)) {
+    randomNumbers.push(randomNumber);
+  }
+}
+
+console.log(randomNumbers);
